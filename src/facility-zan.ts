4cfbc401b7d3f9ac45cbe8fe8b3c723d5b1ed854
Daniel Hawton <daniel@hawton.org>
2023-01-27 23:23:32 -0700

    fix to

--- src/facility-zan.ts
+++ src/facility-zan.ts
@@ -825,7 +825,7 @@ const fac: Facility = {
       sublinks: [
         {
           title: "Oceanic Information",
-          to: "Oceanic",
+          to: { name: "Oceanic" },
         },
       ],
     },
