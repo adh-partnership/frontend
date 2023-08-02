68dea31eb611cec9398aaf1d88f50f2f35bb7bc9
Raajheer1 <raaj.patel229@gmail.com>
2023-08-01 20:10:46 -0500

    Forgot to add that whoops.

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -3,4 +3,4 @@ import fac from "@/facility";
 export const navbarClasses = fac.navbarClasses || "bg-colorado-blue text-white dark:bg-black-deep";
 export const footerClasses = fac.footerClasses || "bg-colorado-blue text-white dark:bg-black-deep";
 export const primaryBackground = fac.primaryBackground || "bg-colorado-blue";
-export const primaryHover = fac.primaryHover || "hover:bg-blue-900";
+export const primaryHover = fac.primaryHover || "bg-colorado-blue hover:bg-blue-900";
