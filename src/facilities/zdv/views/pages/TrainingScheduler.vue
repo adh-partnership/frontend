af7c988ec885d330597b5f0e3b77aaa4bd3ba85c
Celeo <mattboulanger@fastmail.com>
2023-08-10 18:49:36 -0700

    WIP

--- src/facilities/zdv/views/pages/TrainingScheduler.vue
+++ src/facilities/zdv/views/pages/TrainingScheduler.vue
@@ -1,10 +1,12 @@
 <template>
   <h2>Training Scheduler</h2>
-  <div id="calendar" style="height: 800px"></div>
+  <div id="calendar" style="height: 100vh"></div>
 </template>
 
 <script setup lang="ts">
 import "@toast-ui/calendar/dist/toastui-calendar.min.css";
+import "tui-date-picker/dist/tui-date-picker.css";
+import "tui-time-picker/dist/tui-time-picker.css";
 import Calendar, { Options } from "@toast-ui/calendar";
 import { onMounted } from "vue";
 
@@ -15,8 +17,9 @@ onMounted(() => {
     timezone: {
       zones: [
         {
-          timezoneName: "Zulu",
-          displayLabel: "Zulu",
+          timezoneName: "UTC",
+          displayLabel: "UTC",
+          tooltip: "UTC",
         },
       ],
     },
@@ -26,17 +29,41 @@ onMounted(() => {
         name: "Schedule",
       },
     ],
+    week: {
+      taskView: false,
+    },
+    theme: {
+      common: {
+        saturday: { color: "black" },
+        holiday: { color: "black" },
+        today: { color: "black" },
+      },
+      week: {},
+    },
+    isReadOnly: false,
+    useFormPopup: true,
+    useDetailPopup: true,
   };
   // eslint-disable-next-line no-new
   const calendar = new Calendar("#calendar", options);
-  calendar.createEvents([
-    {
-      id: "event1",
-      calendarId: "schedule",
-      title: "Foo",
-      start: "",
-      end: "",
-    },
-  ]);
+
+  calendar.on("beforeCreateEvent", (e) => {
+    if (e.isAllday || !e.start || !e.end) {
+      return;
+    }
+
+    const d1 = e.start as Date;
+    const newStart = new Date(d1.getTime() - d1.getTimezoneOffset() * 60_000);
+    const d2 = e.end as Date;
+    const newEnd = new Date(d2.getTime() - d2.getTimezoneOffset() * 60_000);
+    calendar.createEvents([
+      {
+        ...e,
+        start: newStart,
+        end: newEnd,
+      },
+    ]);
+    calendar.clearGridSelections();
+  });
 });
 </script>
