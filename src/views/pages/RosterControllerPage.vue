e481b72a999eca668ed56ef9bb7f5a97106ad4b8
Raajheer1 <raaj.patel229@gmail.com>
2024-05-26 13:14:41 -0500

    setup frontend

--- src/views/pages/RosterControllerPage.vue
+++ src/views/pages/RosterControllerPage.vue
@@ -34,6 +34,15 @@
             :class="openTab === 3 ? primaryBackground : 'dark:text-white light:text-colorado-blue light:bg-white'"
             @click="toggleTabs(3)"
           >
+            Feedback
+          </a>
+        </li>
+        <li v-if="canActionController()" class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a
+            class="cursor-pointer text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal"
+            :class="openTab === 4 ? primaryBackground : 'dark:text-white light:text-colorado-blue light:bg-white'"
+            @click="toggleTabs(4)"
+          >
             Actions
           </a>
         </li>
@@ -49,6 +58,49 @@
               <TrainingNotes :controller="controller" />
             </div>
             <div v-if="canActionController()" :class="{ hidden: openTab !== 3, block: openTab === 3 }">
+              <div v-if="!feedbacks" class="text-center">
+                <center><Spinner class="fill-colorado-blue" large /></center>
+                <p class="mt-4">Loading feedback...</p>
+              </div>
+              <div v-else>
+                <table class="w-full text-center table table-fixed">
+                  <thead class="border-b-1">
+                  <tr>
+                    <th class="w-24">Position</th>
+                    <th class="w-24">Rating</th>
+                    <th class="hidden md:block">Comment</th>
+                    <th class="w-24">Feedback Date</th>
+                    <th class="w-24 md:hidden">View</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr
+                    v-for="feedback in userFeedbacks"
+                    :key="feedback.id"
+                    class="border-b-8 border-white dark:border-black-light"
+                  >
+                    <td>{{ feedback.position }}</td>
+                    <td>{{ feedback.rating.charAt(0).toUpperCase() + feedback.rating.slice(1) }}</td>
+                    <td class="hidden md:block">{{ feedback.comments }}</td>
+                    <td>{{ new Date(feedback.created_at).toLocaleDateString() }}</td>
+                    <td class="md:hidden">
+                      <button
+                        class="btn bg-blue-800 hover:bg-blue-900 text-white"
+                        type="button"
+                        @click="goTo(feedback.id)"
+                      >
+                        <i class="fa fa-envelope-open w-[15px]" />
+                      </button>
+                    </td>
+                  </tr>
+                  <tr v-if="userFeedbacks.length === 0">
+                    <td colspan="7">There is no feedback.</td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div v-if="canActionController()" :class="{ hidden: openTab !== 4, block: openTab === 4 }">
               <ControllerActions :controller="controller" />
               <ControllerRoles :controller="controller" />
             </div>
