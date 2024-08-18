fd8412e49e126412798a484f7f4fb8935be24b0b
Hayden Young <haydenyounglex@hotmail.com>
2024-08-18 14:38:48 -0400

    re-add LOA and update home page (#441)
    * re-add loa to resources (lol kiki)
    
    * Add Discord, change discription.
    
    * home page updates

--- src/facilities/zid/views/pages/Homepage.vue
+++ src/facilities/zid/views/pages/Homepage.vue
@@ -1,8 +1,11 @@
 <template>
   <h1 class="text-2xl">Welcome to Indy Center</h1>
   <p>
-    Indy Center is an integrated pilot and controller community on the VATSIM network. We simulate immersive aviation and air traffic operations within the Indianapolis ARTCC; approximately 73,000 square miles of airspace over parts of
-    Indiana, Illinois, Kentucky, Ohio, West Virginia, Virginia, and Tennessee. The best part is anyone who is a member of the VATSIM network in good standing can automatically join! Come check out some fun events, learn to control, join a group flight, or just chat with fellow flight sim enthusiasts on our Discord.
+    Indy Center is an integrated pilot and controller community on the VATSIM network. We simulate immersive aviation
+    and air traffic operations within the Indianapolis ARTCC; approximately 73,000 square miles of airspace over parts
+    of Indiana, Illinois, Kentucky, Ohio, West Virginia, Virginia, and Tennessee. The best part is anyone who is a
+    member of the VATSIM network in good standing can automatically join! Come check out some fun events, learn to
+    control, join a group flight, or just chat with fellow flight sim enthusiasts on our Discord.
   </p>
   <div class="grid grid-cols-4 gap-4">
     <div class="card col-span-4 lg:col-span-3 h-min">
@@ -31,7 +34,8 @@
       <div class="card">
         <h2 class="text-xl">Discord</h2>
         <p>
-          <a class="text-alaska-gold" href="https://discord.gg/quYNCbnDfw">Join the Indy Center Discord</a>, the best place to keep up with the community.
+          <a class="text-alaska-gold" href="https://discord.gg/quYNCbnDfw">Join the Indy Center Discord</a>, the best
+          place to keep up with the community.
         </p>
       </div>
       <div class="card">
